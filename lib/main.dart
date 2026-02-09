0ae51626d57791f871cce133144bc72031ec8105
Kamal Ibrahim <kokoangry51@gmail.com>
2026-02-09 17:04:05 +0300

    chore: setup linting and editor config

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Taxi Driver App');
+    return const MaterialApp(title: 'Taxi Driver App');
   }
 }
